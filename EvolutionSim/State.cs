112e93386135171dd8a8e1f1b2028377b5a9a981
KRSDOTA <cpg14aau@uea.ac.uk>
2018-10-11 12:47:18 +0100

    Update EvolutionSim/State.cs
    Added a skeleton class to represent states

---
+++ EvolutionSim/State.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+//Class used to represent the state of an organism
+public class State
+{
+
+    enum States{
+
+        roaming = 0,
+        eating =1,
+        repoducing = 2,
+
+
+    }
+    
+	public State()
+	{
+
+
+	}
+
+
+
+}
