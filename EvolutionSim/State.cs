e66185868d650f58b618162340ba8e9e38ad6e38
KRSDOTA <cpg14aau@uea.ac.uk>
2018-10-11 14:55:54 +0100

    Update EvolutionSim/State.cs
    Started implementing state transition logic, doing a lookup table as a dictonary.

--- EvolutionSim/State.cs
+++ EvolutionSim/State.cs
@@ -1,53 +1,71 @@
 ﻿using System;
-using System.Threading;
+using System.Collections.Generic;
+
+
+//these represent states
+enum States
+{
+
+    roaming = 0,
+    eating = 1,
+    repoducing = 2,
+    seekMate = 3,
+    seekFood = 4,
+}
+
+//these represent the transition psths between states
+enum Action
+{
+    notHungry = 0,
+    hungryRoam = 1,
+    hungryMae = 2,
+
+
+
+}
+
+
 
 //Class used to represent the state of an organism
 public class State
 {
-    States currentAction;
 
 
-    enum States{
+    //a nested and publically avaliable class to determine behaviour of organsim when changing states
+    class StateTransition
+    {
+        readonly States CurrentState;
+        readonly Action action;
 
-        roaming = 0,
-        eating =1,
-        repoducing = 2,
+        public StateTransition(States PassedCurrentState, Action passedAction)
+        {
+            this.CurrentState = PassedCurrentState;
+            this.action = passedAction;
 
+        }
+
+        public override bool Equals(Object obj)
+        {
+            StateTransition other = obj as StateTransition;
+            return other != null && this.CurrentState == other.CurrentState && this.action == other.action;
+
+        }
 
     }
-    
+
+    // represent a transition table as a dictonary
+    Dictionary <StateTransition, States> transitions;
+
+
+
     //Sets each state to roaming by default
 	public State()
 	{
-        this.currentAction = States.roaming;
+       
 
 	}
 
 
-    //method to determine which State our organism needs to be in
-    public void StateLogic()
-    {
-        if (this.currentAction == States.roaming)
-        {
-            //roam
-
-        }
-
-        else if(this.currentAction == States.repoducing)
-        {
-
-            //repoduce
-
-        }
-
-        else if (this.currentAction == States.eating)
-        {
-            //eat
-
-        }
-
-
-    }
-
+ 
 
 }
