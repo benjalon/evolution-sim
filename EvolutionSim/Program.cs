0e24aa6f4016cd1af1fa7d04a76eacf88b23e16f
KRSDOTA <cpg14aau@uea.ac.uk>
2018-10-15 15:48:10 +0100

    Made changes to program.cs
    kek

--- EvolutionSim/Program.cs
+++ EvolutionSim/Program.cs
@@ -1,51 +1,20 @@
 ﻿using System;
-using System.Timers;
 
-/**
- * Main class for handling evolution sim, currently just contains a globally accessable timer with fixed intervals of one second 
- **/
 namespace EvolutionSim
 {
-
-    class Program
+    public class Program
     {
-
-        //need a global timer to keep track of time passing within the system
-       static Timer genericTimer = new Timer();
-
-        static void Main(string[] args)
-
-        {
-       
-
-            genericTimer = new System.Timers.Timer();
-
-            //sets the interval to 1 seconds
-            genericTimer.Interval = 1000;
-
-
-  
-            Console.WriteLine("Hello World");
-
-            Console.ReadLine();
-        }
-
-
-        /**
-         * When the simulation is ran begin the timer
-        **/ 
-        static void Begin()
-        {
-            genericTimer.Enabled = true;
-
-        }
-
-
-        public int updateTime()
+        static void Main()
         {
 
 
-            return 0;
+            //.Console.WriteLine(lmao);
+
+
+            //onsole.WriteLine("sds");
+
+            var graphics = new Graphics();
+            graphics.Run();
         }
     }
 }
