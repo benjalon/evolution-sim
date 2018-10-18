678a1f721f29401e7c54130570c7340a6df4ec45
Ryan <mail@ryanjp.co.uk>
2018-10-18 10:35:03 +0100

    Made collisions more precise.

--- EvolutionSim/Sprite/MovingSprite.cs
+++ EvolutionSim/Sprite/MovingSprite.cs
@@ -44,15 +44,14 @@ namespace EvolutionSim
         {
             AttemptDirectionChange(gameTime);
 
-            var plannedPosition = new Point(Rectangle.X + _movement.X, Rectangle.Y + _movement.Y);
+            var plannedPosition = new Rectangle(Rectangle.X + _movement.X, Rectangle.Y + _movement.Y, Rectangle.Width, Rectangle.Height);
             if (CheckOutOfBounds(bounds, plannedPosition) || CheckCollisions(colliders, plannedPosition))
             {
                 return; // Give up with this movement and wait for either the collider to move or the next direction change
             }
 
             // Move the sprite
-            _rectangle.X = plannedPosition.X;
-            _rectangle.Y = plannedPosition.Y;
+            _rectangle = plannedPosition;
         }
 
         /// <summary>
@@ -79,12 +78,12 @@ namespace EvolutionSim
         /// <param name="colliders">Elements this move could possibly collide with</param>
         /// <param name="plannedPosition">The planned new position</param>
         /// <returns>True for will collide, false for will not collide</returns>
-        private bool CheckCollisions(List<Sprite> colliders, Point plannedPosition)
+        private bool CheckCollisions(List<Sprite> colliders, Rectangle plannedPosition)
         {
             var collisionRectangle = Rectangle.Empty;
             foreach (var collider in colliders)
             {
-                if (collider.Rectangle.Contains(plannedPosition))
+                if (collider != this && collider.Rectangle.Intersects(plannedPosition))
                 {
                     collisionRectangle = collider.Rectangle; // Collision found
                     break;
@@ -100,10 +99,10 @@ namespace EvolutionSim
         /// <param name="bounds">The screen bounds</param>
         /// <param name="plannedPosition">The planned movement</param>
         /// <returns>True for will be out of bounds, false for will not be out of bounds</returns>
-        private bool CheckOutOfBounds(Rectangle bounds, Point plannedPosition)
+        private bool CheckOutOfBounds(Rectangle bounds, Rectangle plannedPosition)
         {
-            var outOfBoundsLeftRight = plannedPosition.X <= bounds.Left || plannedPosition.X >= bounds.Right;
-            var outOfBoundsTopBottom = plannedPosition.Y <= bounds.Top || plannedPosition.Y >= bounds.Bottom;
+            var outOfBoundsLeftRight = plannedPosition.Left <= bounds.Left || plannedPosition.Right >= bounds.Right;
+            var outOfBoundsTopBottom = plannedPosition.Top <= bounds.Top || plannedPosition.Bottom >= bounds.Bottom;
             return outOfBoundsLeftRight || outOfBoundsTopBottom;
         }
     }
