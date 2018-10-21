91c21f2ec6ce3d3c3824c178992946112c176900
KRSDOTA <cpg14aau@uea.ac.uk>
2018-10-21 15:16:31 +0100

    Update EvolutionSim/StateMachine.cs

--- EvolutionSim/StateMachine.cs
+++ EvolutionSim/StateMachine.cs
@@ -11,83 +11,119 @@ namespace EvolutionSim
     class StateMachine
     {
 
-        //list of constructed organisms which are currently in game
-        List<Organism> liveOrganisms = new List<Organism>();
-        
 
-    
-    public State performAction(Organism _passedOrganism)
+        //this method provides the logic for state transitions
+        public void performAction(Organism _passedOrganism)
         {
-                //test the organisms current attributes
-                //by switching on the current state
+            //test the organisms current attributes
+            //by switching on the current state
 
-                States organismState = (States)_passedOrganism._state.CurrentState;
-                Action neededAction;
-                State newState;
-
-                //then we're in roaming
-                if (organismState == States.Roaming)
-                {
-                    //hunger falls below a certain percentage or value
-            
+            States organismState = (States)_passedOrganism._state.CurrentState;
+            Action neededAction;
+            State newState;
 
 
+            switch (organismState)
+            {
 
+                case States.Roaming:
 
-                }
+                    //if food < 50 then go to seek food, contuine to seek food until back to 80% full?
+                    //diferent food sources have different values
 
+                    if (_passedOrganism._attributes._hunger < 0.4)
+                    {
 
-                else if(organismState == States.Eating)
-                {
-
-
+                        //then move into the seek food state
+                        _passedOrganism._state.MoveState(Action.HungryRoam);
 
 
 
-                }
+                    }
 
-                else if (organismState == States.SeekFood)
-                {
-
-
-
-
-
-                }
-
-                else if (organismState == States.SeekMate)
-                {
+                    if (_passedOrganism._attributes._hunger > 0.8)
+                    {
+                        //go find a mate
+                        _passedOrganism._state.MoveState(Action.HungryMate);
 
 
+                    }
+
+                    else
+                    {
+                        Random newRand = new Random();
+
+                        int MateOrEat = newRand.Next(0, 1);
+
+                        if (MateOrEat == 1)
+                        {
+                            _passedOrganism._state.MoveState(Action.HungryMate);
+
+                        }
+                        else
+                        {
+
+                            _passedOrganism._state.MoveState(Action.HungryRoam);
+
+                        }
+
+                    }
+
+                    break;
+
+                //this code block handles logic when organism is the middle of eating
+                case States.Eating:
+
+                    //if there is food in food source then contuine eating
+
+                    //if food hungry is maxed out (100) then stop eating && move back into roaming
+                    if (_passedOrganism._attributes._hunger == 1.0)
+                    {
+
+                        _passedOrganism._state.MoveState(Action.NotHungry);
+
+                    }
+
+                    break;
+
+                //organism needs way of tracking other organisms of the same species
+                //
+                case States.SeekMate:
+
+                    break;
+
+                case States.SeekFood:
+
+                    // 1) call tracking method, 
+                    // 2) if tracking method return true then the organism moves over to food and transitions into the eating state:
+
+                    _passedOrganism._state.MoveState(Action.FoodFound);
+
+                    break;
+
+
+                case States.Mating:
+
+                    break;
+
+
+                default:
+
+                    break;
 
 
 
-                }
-
-
-                else 
-                {
+            }
 
 
 
-
-
-                }
-
-
-
-
-
-
-
-
-
-
-
-            return newState;
-
-   
         }
 
-  
+
+        //needs some sort of behaviour method
+        //need to tie in graphics to a behviour method of some sort 
+
+
+
+    }
 }
