2df27cd2b79990992a2426ad409f1a0b7d3a4a20
KRSDOTA <cpg14aau@uea.ac.uk>
2019-01-09 15:24:07 +0000

    Create RayPathfinding.cs

---
+++ EvolutionSim/TileGrid/Pathfinding/RayPathfinding.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EvolutionSim.TileGrid.Pathfinding
+{
+
+    /// <summary>
+    /// This gives us simplified path finding
+    /// </summary>
+    class RayPathfinding
+    {
+
+
+
+
+    }
+}
